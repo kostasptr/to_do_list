ce3a42d474b6e0533e6509d464e799fd3b0fb94b
kostasptr <kostas-ptr@hotmail.com>
2021-12-07 15:57:08 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// let eventkey = document.querySelector(".subText");
+
+// document.querySelector("#fname").addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//     addNote();
+//   }
+// });
+
+// document.getElementById("fname").addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//     addNote();
+//   }
+// });
+
+// document.querySelector("#fname").addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//     let x = document.querySelector("#fname").value;
+// //        console.log(x);
+//     console.log("console.log(x); (1)");
+//   }
+// });
+
+document.querySelector("#fname").addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    addNote();
+    // const usersInput = document.querySelector("#fname");
+  }
+});
+
+function addNote(){
+  let x =  document.querySelector("#fname").value;
+  let divN = document.createElement("div");
+  let para = document.createElement("p");
+  let paraText = document.createTextNode(x);
+  let but = document.createElement("button");
+  let y = document.querySelector(".test");
+
+  divN.classList.add("notesList");
+  para.classList.add("notesInput");
+  but.classList.add("deleteBut");
+
+  but.setAttribute("onclick", "deleFunc(this)");
+
+  divN.appendChild(para);
+  para.appendChild(paraText);
+  divN.appendChild(but);
+  y.appendChild(divN);
+}
+
+function deleFunc(el){
+  var element = el;
+  element.parentNode.remove();
+}
